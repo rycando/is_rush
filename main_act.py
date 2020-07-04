f2e855f864a8df3158846ddf40792c54f60544eb
rycando <rycando@naver.com>
2020-07-04 12:21:04 +0900

    auth comp

--- main_act.py
+++ main_act.py
@@ -1,9 +1,19 @@
 import requests
-import requests.auth
+import json
 
 UID = ""
 SECRET = ""
 
-def get_token(code):
-    client_auth = requests.auth.HTTPBasicAuth(UID, SECRET)
-    post_data = {""}
+oauth_data = {
+    'grant_type' : 'client_credentials',
+    'client_id' : UID,
+    'client_secret' : SECRET,
+}
+oauth_url = "https://api.intra.42.fr/oauth/token"
+api_url = "https://api.intra.42.fr/v2/"
+
+oauth_request = requests.post(oauth_url, data = oauth_data)
+print(oauth_request.json())
+access_token = oauth_request.json()['access_token']
+print(requests.get(f'{api_url}cursus', params={'access_token' : access_token}).json())
+#29
