320a80735b02e393156c0c72803c1449344b3a3c
Ryunyeong Kim <ryukim@c1r7s7.42seoul.kr>
2020-07-04 12:22:00 +0900

    mid

--- main_act.py
+++ main_act.py
@@ -1,5 +1,4 @@
-import requests
-import requests.auth
+import requests, json
 
 UID = ""
 SECRET = ""
@@ -7,3 +6,13 @@ SECRET = ""
 def get_token(code):
     client_auth = requests.auth.HTTPBasicAuth(UID, SECRET)
     post_data = {""}
+
+oauth_data = {
+    'grant_type': 'client_credentials',
+    'client_id': UID,
+    'client_secret': SECRET,
+}
+oauth_url = "https://api.intra.42.fr/oauth/token"
+api_url = "https://api.intra.42.fr/v2/"
+
+oauth_request = requests.post(oauth_url, data=oauth_data)
